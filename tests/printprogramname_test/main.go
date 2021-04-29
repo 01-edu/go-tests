3ada9cfb989a589c0dd4107666901b7cf2a589ea
xpetit <32063953+xpetit@users.noreply.github.com>
2021-04-29 14:56:32 +0200

    Fix printprogramname

--- tests/printprogramname_test/main.go
+++ tests/printprogramname_test/main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
+	"errors"
 	"os"
 	"os/exec"
 
 	"github.com/01-edu/go-tests/lib/challenge"
 )
 
+func panicIfNot(target, err error) {
+	if err != nil && err != target && !errors.Is(err, target) {
+		panic(err)
+	}
+}
+
 var oldName = "exe"
 
 func test(name string) {
-	if err := os.Rename(oldName, name); err != nil {
-		challenge.Fatalln(err)
-	}
+	panicIfNot(nil, exec.Command("go", "build", "-o", oldName, "student/printprogramname").Run())
+	panicIfNot(nil, (os.Rename(oldName, name)))
 	b, err := exec.Command("./" + name).CombinedOutput()
-	if err != nil {
-		challenge.Fatalln(err)
-	}
+	panicIfNot(nil, err)
 	if string(b) != name+"\n" {
 		challenge.Fatalln("Failed to print the program name :", string(b))
 	}
