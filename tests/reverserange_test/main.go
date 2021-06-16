f7c3874d18961b0819d1ce73eae979fee5df8425
lee <lee-dasilva@hotmail.com>
2021-06-16 11:56:50 +0100

    exam03 review and test

--- tests/reverserange_test/main.go
+++ tests/reverserange_test/main.go
@@ -8,15 +8,19 @@ import (
 )
 
 func main() {
+	table := [][]string{
+		{"2", "1", "3"},
+		{"-1", "2"},
+		{"0", "-3"},
+		{"1", "nan"},
+		{"nan", "b"},
+	}
 	for i := 0; i < 10; i++ {
 		start := random.IntBetween(-20, 20)
 		end := random.IntBetween(-20, 20)
-		challenge.Program("reverserange", strconv.Itoa(start), strconv.Itoa(end))
+		table = append(table, []string{strconv.Itoa(start), strconv.Itoa(end)})
 	}
-	challenge.Program("reverserange", "2", "1", "3")
-	challenge.Program("reverserange", "a", "1")
-	challenge.Program("reverserange", "1", "b")
-	challenge.Program("reverserange", "1", "nan")
-	challenge.Program("reverserange", "nan", "b")
-	challenge.Program("reverserange")
+	for _, v := range table {
+		challenge.Program("reverserange", v...)
+	}
 }
