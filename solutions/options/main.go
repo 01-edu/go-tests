ba5297487ef034b2fbcc9a20f6622b802f144bd0
lee <lee-dasilva@hotmail.com>
2021-06-18 18:58:36 +0100

    review: remove function to inlice

--- solutions/options/main.go
+++ solutions/options/main.go
@@ -3,40 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 	"unicode"
 )
 
-func printHelper() {
-	fmt.Println("options: abcdefghijklmnopqrstuvwxyz")
-	os.Exit(0)
-}
-
-func printOptions(op []string) {
-
-}
-
 func main() {
 	if len(os.Args) < 2 {
-		printHelper()
+		fmt.Println("options: abcdefghijklmnopqrstuvwxyz")
+		return
 	}
 
-	options := strings.Split("00000000000000000000000000000000", "")
+	var options [32]bool
 	for _, v := range os.Args {
 		if len(v) < 2 {
 			return
 		}
 		if v[0] == '-' {
 			if v[1] == 'h' {
-				printHelper()
+				fmt.Println("options: abcdefghijklmnopqrstuvwxyz")
+				return
 			}
 			// fill options
 			for _, r := range v[1:] {
 				if !unicode.Is(unicode.Latin, r) {
 					fmt.Println("Invalid Option")
-					os.Exit(0)
+					return
 				}
-				options['z'-r+6] = "1"
+				options['z'-r+6] = true
 			}
 		}
 	}
@@ -44,7 +36,11 @@ func main() {
 		if i%8 == 0 && i != 0 {
 			fmt.Print(" ")
 		}
-		fmt.Print(v)
+		if v {
+			fmt.Print(1)
+		} else {
+			fmt.Print(0)
+		}
 	}
 	fmt.Println()
 }
