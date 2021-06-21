589aa95e4841475e99960f55d6f96724154607f9
OGordoo <marcosafonso8@hotmail.com>
2021-06-21 11:52:38 +0100

    printmemory change exercise

--- tests/printmemory_test/main.go
+++ tests/printmemory_test/main.go
@@ -9,16 +9,14 @@ import (
 )
 
 func main() {
-	var table [10]int
+	table := [10]int{}
 
 	for j := 0; j < 5; j++ {
-		for i := 0; i < 10; i++ {
-			table[i] = random.IntBetween(0, 1000)
+		for i := 0; i < random.IntBetween(7, 10); i++ {
+			table[i] = random.IntBetween(13, 126)
 		}
 		challenge.Function("PrintMemory", student.PrintMemory, solutions.PrintMemory, table)
 	}
 	table2 := [10]int{104, 101, 108, 108, 111, 16, 21, 42}
 	challenge.Function("PrintMemory", student.PrintMemory, solutions.PrintMemory, table2)
 }
-
-// TODO: this can be simplified a lot
