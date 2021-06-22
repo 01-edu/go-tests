f37ca10aa763d9b2a01ddaa56e528d3ef53ec3d2
Chris <you@example.com>
2021-06-22 03:51:10 +0100

    insertion of tests of the readme. Reduction of random tests from 25 to 5.

--- tests/gcd_test/main.go
+++ tests/gcd_test/main.go
@@ -9,13 +9,17 @@ import (
 
 func main() {
 	args := [][]string{
+		{"42", "10"},
+		{"42", "12"},
+		{"14", "77"},
+		{"17", "3"},
 		{"23"},
 		{"12", "23"},
 		{"25", "15"},
 		{"23043", "122"},
 		{"11", "77"},
 	}
-	for i := 0; i < 25; i++ {
+	for i := 0; i < 5; i++ {
 		a := strconv.Itoa(random.IntBetween(1, 100000))
 		b := strconv.Itoa(random.IntBetween(1, 100))
 		args = append(args, []string{a, b})
