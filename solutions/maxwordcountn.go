dd3975271a8b163af18ae74d8d75dcee3b8694ab
daggerron <cmalfroy@student.42.fr>
2021-07-30 18:59:30 +0200

    New exercice proposal to use maps in the piscine

---
+++ solutions/maxwordcountn.go
@@ -0,0 +1,39 @@
+package solutions
+
+import (
+	"sort"
+	"strings"
+)
+
+type KeyValue struct {
+        Key   string
+        Value int
+}
+
+
+func MaxWordCountN(text string, n int) map[string]int {
+	wordOccurences := make(map[string]int)
+	for _, word := range strings.Split(text, " ") {
+			wordOccurences[word]++
+	}
+
+	var keyValSlice []KeyValue
+	for k, v := range wordOccurences {
+			keyValSlice = append(keyValSlice, KeyValue{k, v})
+	}
+
+	sort.Slice(keyValSlice, func(i, j int) bool {
+			if keyValSlice[i].Value == keyValSlice[j].Value {
+				return keyValSlice[i].Key < keyValSlice[j].Key
+			} else {
+			  return keyValSlice[i].Value > keyValSlice[j].Value
+			}
+
+	})
+
+	nMaxWordOccurences := make(map[string]int)
+	for _, kv := range keyValSlice[0:n] {
+		nMaxWordOccurences[kv.Key] = kv.Value
+	}
+	return nMaxWordOccurences
+}
