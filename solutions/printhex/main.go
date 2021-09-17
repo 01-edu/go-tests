d53f02a9e84c7db23c3aa8b6131ad9abdc970708
OGordoo <marcosafonso8@hotmail.com>
2021-09-17 14:42:03 +0100

    printhex small fix

--- solutions/printhex/main.go
+++ solutions/printhex/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	if len(os.Args) == 2 {
-		if i, err := strconv.Atoi(os.Args[1]); err == nil && i > 0 {
+		if i, err := strconv.Atoi(os.Args[1]); err == nil && i >= 0 {
 			fmt.Printf("%x\n", i)
 		} else {
 			fmt.Println("ERROR")
