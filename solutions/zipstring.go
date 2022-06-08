6accce99eafb0d2c81742dd0b6148529b489ad72
Mohamed Zaboub <mozaboub@gmail.com>
2022-06-08 10:50:59 +0100

    add(solutions): zipstring proposed solution

---
+++ solutions/zipstring.go
@@ -0,0 +1,28 @@
+package solutions
+
+import (
+	"strconv"
+)
+
+func countDuplication(s string, i int) int {
+	var count int = 0
+	for _, v := range s[i:] {
+		if v == rune(s[i]) {
+			count++
+		} else {
+			break
+		}
+	}
+	return count
+}
+
+func ZipString(s string) string {
+	var result string
+	i := 0
+	for i < len(s) {
+		counter := countDuplication(s, i)
+		result = result + strconv.Itoa(counter) + string(s[i])
+		i += int(counter)
+	}
+	return result
+}
