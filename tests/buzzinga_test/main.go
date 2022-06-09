22a345d8d5d8aa789ccb946ac5b274e123125968
zainabdnaya <diyanazizo13@gmail.com>
2022-06-09 12:10:25 +0100

    freat: add buzzinga_test

--- tests/buzzinga_test/main.go
+++ tests/buzzinga_test/main.go
@@ -1,26 +1,16 @@
 package main
 
 import (
-	student "student"
+    student "student"
 
-	"github.com/01-edu/go-tests/lib/challenge"
+    "github.com/01-edu/go-tests/lib/challenge"
+    "github.com/01-edu/go-tests/solutions"
 )
 
-func solutions(n int32) string {
-	solutions := map[int32]string{
-		15: "*\n*\nBuz\n*\nzinga\nBuz\n*\n*\nBuz\nzinga\n*\nBuz\n*\n*\nBuzzinga\n",
-		10: "*\n*\nBuz\n*\nzinga\nBuz\n*\n*\nBuz\nzinga\n",
-		5:  "*\n*\nBuz\n*\nzinga\n",
-		4:  "*\n*\nBuz\n*\n",
-		20: "*\n*\nBuz\n*\nzinga\nBuz\n*\n*\nBuz\nzinga\n*\nBuz\n*\n*\nBuzzinga\n*\n*\nBuz\n*\nzinga\n",
-	}
-	return solutions[n]
-}
-
 func main() {
-	challenge.Function("BuzZinga", student.BuzZinga, solutions, 20)
-	challenge.Function("BuzZinga", student.BuzZinga, solutions, 15)
-	challenge.Function("BuzZinga", student.BuzZinga, solutions, 10)
-	challenge.Function("BuzZinga", student.BuzZinga, solutions, 5)
-	challenge.Function("BuzZinga", student.BuzZinga, solutions, 4)
+    challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, 20)
+    challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, 15)
+    challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, 10)
+    challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, 5)
+    challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, 4)
 }
