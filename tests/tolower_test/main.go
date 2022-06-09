c005efab9378b4fb2d326f689e61373e5e972786
zainabdnaya <diyanazizo13@gmail.com>
2022-06-09 23:23:25 +0100

    feat: add solution of VowelsIndex.go

--- tests/tolower_test/main.go
+++ tests/tolower_test/main.go
@@ -1,17 +1,1 @@
-package main
-
-import (
-	student "student"
-
-	"github.com/01-edu/go-tests/lib/challenge"
-	"github.com/01-edu/go-tests/lib/chars"
-	"github.com/01-edu/go-tests/lib/random"
-	"github.com/01-edu/go-tests/solutions"
-)
-
-func main() {
-	table := append(random.StrSlice(chars.ASCII), "Hello! How are you?")
-	for _, arg := range table {
-		challenge.Function("ToLower", student.ToLower, solutions.ToLower, arg)
-	}
-}
+/usr/local/Cellar/go/1.18.3/libexec
