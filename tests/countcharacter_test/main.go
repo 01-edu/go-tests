f7d65f0330782eee7526a41636c2207880c39312
zainabdnaya <diyanazizo13@gmail.com>
2022-06-10 17:53:40 +0100

    feat:Add The Countchar test

--- tests/countcharacter_test/main.go
+++ tests/countcharacter_test/main.go
@@ -3,23 +3,15 @@ package main
 import (
 	student "student"
 
-	"github.com/01-edu/go-tests/lib/base"
 	"github.com/01-edu/go-tests/lib/challenge"
 	"github.com/01-edu/go-tests/solutions"
 )
 
-func CountChar(str string, c rune) int {
-	var count int
-	for _, v := range str {
-		if rune(v) == c {
-			count++
-		}
-	}
-	return count
-}
-
 func main() {
-	challenge.Function("CountChar", student.CountChar, CountChar, "hello", 'e')
-	challenge.Function("CountChar", student.CountChar, CountChar, " 	", ' ')
-	challenge.Function("CountChar", student.CountChar, CountChar, "the 7 deadly sins", '7')
+	challenge.Function("CountChar", solutions.CountChar, student.CountChar, "hello", 'e')
+	challenge.Function("CountChar", solutions.CountChar, student.CountChar, "     ", ' ')
+	challenge.Function("CountChar", solutions.CountChar, student.CountChar, "the 7 deadly sins", '7')
+	challenge.Function("CountChar", solutions.CountChar, student.CountChar, "the 7 deadly sins", 's')
+	challenge.Function("CountChar", solutions.CountChar, student.CountChar, "the 7 deadly sins", 'd')
+	challenge.Function("CountChar", solutions.CountChar, student.CountChar, "", 'i')
 }
