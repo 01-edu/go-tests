9d2e20ae30ce699a63371013cf720eb5004e8f26
hamza <hamzaelkhatri@gmail.com>
2022-06-11 14:21:57 +0100

    add(test): for alpha position

---
+++ solutions/alphaposition.go
@@ -0,0 +1,10 @@
+package solutions
+
+func AlphaPosition(c rune) int{
+	if c >= 'a' && c <= 'z' {
+		return int(c - 'a' + 1)
+	} else if c >= 'A' && c <= 'Z' {
+		return int(c - 'A' + 1)
+	}
+	return -1
+}
