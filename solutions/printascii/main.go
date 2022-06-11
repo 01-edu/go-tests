9541c621c45defc3172678b280650b113efd05da
hamza <hamzaelkhatri@gmail.com>
2022-06-11 15:17:13 +0100

    add(test): for printascii

---
+++ solutions/printascii/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+import "os"
+
+func main(){
+	args := os.Args
+	if len(args) != 2 || len(args[1]) != 1 {
+		fmt.Println()
+		return
+	}
+	ascii := int(args[1][0])
+	if ascii >= 65 && ascii <= 90 || ascii >= 97 && ascii <= 122 {
+		fmt.Println(ascii)
+		return
+	}
+}
