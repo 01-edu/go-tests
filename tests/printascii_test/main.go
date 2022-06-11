c7fcfe2dbebac6aee82449fd3bd8e793dc527b31
hamza <hamzaelkhatri@gmail.com>
2022-06-11 15:17:13 +0100

    add(test): for printascii

---
+++ tests/printascii_test/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/01-edu/go-tests/lib/challenge"
+)
+
+func main() {
+	args := [][]string{
+		{"S"},
+		{"One", "ring!"},
+		{"A"},
+		{"E"},
+		{"z"},
+		{"testing spaces and #!*"},
+		{"more", "than", "three", "arguments"},
+		{"W"},
+		{"1"},
+		{""},
+	}
+
+	for _, v := range args {
+		challenge.Program("printascii", v...)
+	}
+}
