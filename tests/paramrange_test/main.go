30cfb90efc987c89124d9e60fb4085a33715a834
hamza <hamzaelkhatri@gmail.com>
2022-06-14 12:49:38 +0100

    test(paramrange):add test & solution

---
+++ tests/paramrange_test/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/01-edu/go-tests/lib/challenge"
+)
+
+func main() {
+	args := [][]string{
+		{"2", "5", "u", "19"},
+		{"2"},
+		{"1", "2", "3", "5", "7", "24"},
+		{"6", "12", "24"},
+		{""},
+		{"-10", "10", "20", "30", "40", "50"},
+		{"Hello world"},
+	}
+	for _, v := range args {
+		challenge.Program("paramrange", v...)
+	}
+	challenge.Program("paramrange")
+}
