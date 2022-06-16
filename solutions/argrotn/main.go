d72990e5d1a49d4d0cd754fbec5c3a5d3b190b7e
hamza <hamzaelkhatri@gmail.com>
2022-06-16 20:30:45 +0100

    fix(rot_args_n): fix loop

--- solutions/argrotn/main.go
+++ solutions/argrotn/main.go
@@ -4,18 +4,6 @@ import "fmt"
 import "os"
 import "strconv"
 
-func rotationByN(s []string, n int) []string {
-
-	str := make([]string, 0)
-	if n == 0 {
-		return s
-	}
-	for i := 0; i < len(s); i++ {
-		str = append(str, string(s[(i+n)%len(s)]))
-	}
-	return str
-}
-
 func main() {
 	if len(os.Args) < 3 {
 		fmt.Println("")
@@ -29,10 +17,10 @@ func main() {
 		fmt.Println("Error")
 		return
 	}
-	result := rotationByN(args, n)
-	for _, v := range result {
-		fmt.Print(v)
-		if v != result[len(result)-1] {
+
+	for i := 0; i < len(args); i++ {
+		fmt.Print(string(args[(i+n)%len(args)]))
+		if  i < len(args)-1 {
 			fmt.Print(" ")
 		}
 	}
