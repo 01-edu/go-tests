f9e0ccbf4842cd43c265d6ec524f7de5789a219e
zainabdnaya <diyanazizo13@gmail.com>
2022-06-16 11:26:35 +0100

    feat: Add Solution and Test of SameCase

---
+++ tests/samecase_test/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/01-edu/go-tests/lib/challenge"
+)
+
+func main() {
+
+	args := [][]string{
+		{"A", "B"},
+		{"a", "b"},
+		{"A", "b"},
+		{"A", "1"},
+		{" "},
+		{"Hello", "World"},
+		{""},
+		{"1", "2"},
+	}
+	for _, arg := range args {
+		challenge.Program("samecase", arg...)
+	}
+}
+/// rah it work n
