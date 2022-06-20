84d241c9136a90f32edce094d49af24682a795fe
zainabdnaya <diyanazizo13@gmail.com>
2022-06-20 22:26:59 +0100

    buzzinga-test

---
+++ tests/buzzinga_test/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	student "student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/solutions"
+)
+
+func main() {
+	challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, 20)
+	challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, 15)
+	challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, -125)
+	challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, 10)
+	challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, 5)
+	challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, 4)
+	challenge.Function("BuzZinga", student.BuzZinga, solutions.BuzZinga, -9)
+}
