15458bc651da5f9b29c026b85fab94fa0ab21dc1
hamza <hamzaelkhatri@gmail.com>
2022-06-20 22:48:12 +0100

    add empty array

--- solutions/countnegative.go
+++ solutions/countnegative.go
@@ -2,6 +2,9 @@ package solutions
 
 func CountNegative(numbers []int) int {
 	count := 0
+	if len(numbers) == 0 {
+		return count
+	}
 	for _, number := range numbers {
 		if number < 0 {
 			count++
