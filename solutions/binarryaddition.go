9fa0e18fe7074caa29bb916238ee757183df490c
Hamza elkhatri <40549481+Hamzaelkhatri@users.noreply.github.com>
2022-06-21 11:16:10 +0100

    Update binarryaddition.go

--- solutions/binarryaddition.go
+++ solutions/binarryaddition.go
@@ -1,40 +1,27 @@
 package solutions
 
 func BinaryAddition(a, b int) []int {
-	var binaryA []int
-	var binaryB []int
-	if a < 0 || b < 0 {
-		return nil
-	}
-	for a > 0 {
-		binaryA = append(binaryA, a%2)
-		a = a / 2
-	}
-	for b > 0 {
-		binaryB = append(binaryB, b%2)
-		b = b / 2
-	}
-	var sum []int
-	carry := 0
-	i := 0
-	for i < len(binaryA) || i < len(binaryB) {
-		if i < len(binaryA) {
-			carry += binaryA[i]
+	var result []int
+	var carry int = 0
+	for a > 0 || b > 0 {
+		var sum int
+		if a > 0 {
+			sum = a % 2
+			a = a / 2
 		}
-		if i < len(binaryB) {
-			carry += binaryB[i]
+		if b > 0 {
+			sum += b % 2
+			b = b / 2
 		}
-		sum = append(sum, carry%2)
-		carry = carry / 2
-		i++
+		sum += carry
+		carry = sum / 2
+		result = append(result, sum%2)
 	}
 	if carry > 0 {
-		sum = append(sum, carry)
-	} else if carry == 0 {
-		sum = append(sum, 0)
+		result = append(result, carry)
 	}
-	for i, j := 0, len(sum)-1; i < j; i, j = i+1, j-1 {
-		sum[i], sum[j] = sum[j], sum[i]
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
 	}
-	return sum
+	return result
 }
