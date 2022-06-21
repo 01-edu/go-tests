4881cc53cf2d1c900a686987dc98992edaf2c1eb
zainabdnaya <diyanazizo13@gmail.com>
2022-06-21 21:40:57 +0100

    add test & solution

---
+++ solutions/unzipstring.go
@@ -0,0 +1,25 @@
+package solutions
+
+import "strconv"
+
+func Unzipstring(str string) string {
+    var result string
+	if len(str) == 0{
+		return "Invalid output"
+	}
+    num, err := strconv.Atoi(string(str[0]))
+    if err != nil {
+        return "Invalid output"
+    }
+    for i := 0; i < num; i++ {
+        result += string(str[1])
+    }
+    if len(str) > 2 {
+        if Unzipstring(string(str[2:])) != "Invalid output" {
+            result += Unzipstring(string(str[2:]))
+        } else {
+            return "Invalid output"
+        }
+    }
+    return result
+}
