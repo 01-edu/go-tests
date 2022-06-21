10420260d31c2c9b51be47ce5a2cc226189d1861
zainabdnaya <diyanazizo13@gmail.com>
2022-06-21 22:16:47 +0100

    -

--- tests/unzipstring_test/main.go
+++ tests/unzipstring_test/main.go
@@ -7,9 +7,20 @@ import (
 	"github.com/01-edu/go-tests/solutions"
 )
 
-
 func main() {
-	arr := []string{"6H8a", "3p6i1W", "2O5u2H0e", "2d", "2a 6p8f", "2p4;7g", "2t4dD", "82t4D", "2k5%9.", "", "5w6	2t",}
+	arr := []string{
+		"6H8a",
+		"3p6i1W",
+		"2O5u2H0e",
+		"2d",
+		"2a 6p8f",
+		"2p4;7g",
+		"2t4dD",
+		"82t4D",
+		"2k5%9.",
+		"",
+		"5w6	2t"
+	}
 	for _, s := range arr {
 		challenge.Function("Unzipstring", student.Unzipstring, solutions.Unzipstring, s)
 	}
