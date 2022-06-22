d71320234564db208831cda6541d965199e33404
estlop <lopezesteban8@gmail.com>
2022-06-22 13:45:00 +0100

    test: Add test for pingpong subject

---
+++ tests/pingpong_test/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/01-edu/go-tests/lib/challenge"
+
+func main() {
+	args := [][]string{
+		{""},
+		{"0"},
+		{"3", "5"},
+		{"5"},
+		{"9"},
+		{"24"},
+		{"99"},
+	}
+	for _, arg := range args {
+		challenge.Program("pingpong", arg...)
+	}
+}
