45203527580165ef42c5882e6efd824f11aeaffa
hamza <hamzaelkhatri@gmail.com>
2022-06-27 10:35:53 +0100

    fix: space between args

--- solutions/swapargs/main.go
+++ solutions/swapargs/main.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-	"github.com/01-edu/z01"
+	"fmt"
 	"os"
 )
 
 func main() {
 	if len(os.Args) != 3 {
-		z01.PrintRune('\n')
+		fmt.Println("")
 		return
 	}
-	for i := 0; i < len(os.Args[2]); i++ {
-		z01.PrintRune(rune(os.Args[2][i]))
-	}
-	for i := 0; i < len(os.Args[1]); i++ {
-		z01.PrintRune(rune(os.Args[1][i]))
-	}
-	z01.PrintRune('\n')
+	fmt.Println(os.Args[2] + " " + os.Args[1])
 }
