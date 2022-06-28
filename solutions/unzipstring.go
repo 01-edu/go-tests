31b048d69a7098c25efb42890a37e835362e561b
zainabdnaya <diyanazizo13@gmail.com>
2022-06-28 21:08:16 +0100

    Correction

--- solutions/unzipstring.go
+++ solutions/unzipstring.go
@@ -3,23 +3,23 @@ package solutions
 import "strconv"
 
 func Unzipstring(str string) string {
-    var result string
-    if  len(str) == 0 || len(str) < 2 {
-        return "Invalid output"
-    }
-    num, err := strconv.Atoi(string(str[0]))
-    if err != nil || ( str[1] >= '0' && str[1] <= '9') {
-        return "Invalid output"
-    }
-    for i := 0; i < num; i++ {
-        result += string(str[1])
-    }
-    if len(str) > 2 {
-        if Unzipstring(string(str[2:])) != "Invalid output" {
-            result += Unzipstring(string(str[2:]))
-        } else {
-            return "Invalid output"
-        }
-    }
-    return result
+	var result string
+	if len(str) == 0 || len(str) < 2 {
+		return "Invalid output"
+	}
+	num, err := strconv.Atoi(string(str[0]))
+	if err != nil || (str[1] >= '0' && str[1] <= '9') {
+		return "Invalid output"
+	}
+	for i := 0; i < num; i++ {
+		result += string(str[1])
+	}
+	if len(str) > 2 {
+		if Unzipstring(string(str[2:])) != "Invalid output" {
+			result += Unzipstring(string(str[2:]))
+		} else {
+			return "Invalid output"
+		}
+	}
+	return result
 }
