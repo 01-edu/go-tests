3da7f86ef79c3511f7b0b7378d3f5cd141486f9e
estlop <lopezesteban8@gmail.com>
2022-06-28 14:25:22 +0100

    fix: Remove random tests

--- tests/pingpong_test/main.go
+++ tests/pingpong_test/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"strconv"
-
 	"github.com/01-edu/go-tests/lib/challenge"
-	"github.com/01-edu/go-tests/lib/random"
 )
 
 func main() {
@@ -17,10 +14,6 @@ func main() {
 		{"24"},
 		{"99"},
 		{"100"},
-	}
-	for i := 0; i < 10; i++ {
-		rand := []string{strconv.Itoa(random.IntBetween(0, 99))}
-		args = append(args, rand)
 	}
 	for _, arg := range args {
 		challenge.Program("pingpong", arg...)
