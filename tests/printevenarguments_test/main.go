e965e54c68afec985c1e176312b54df6214a055b
estlop <lopezesteban8@gmail.com>
2022-06-29 13:09:39 +0100

    Correct wrong program name

--- tests/printevenarguments_test/main.go
+++ tests/printevenarguments_test/main.go
@@ -13,6 +13,6 @@ func main() {
 		{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"},
 	}
 	for _, arg := range table {
-		challenge.Program("concatenate", arg...)
+		challenge.Program("printevenarguments", arg...)
 	}
 }
