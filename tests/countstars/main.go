b1c6eb1cf9e7425dfd1612d1336343e46c065a9a
zainabdnaya <diyanazizo13@gmail.com>
2022-06-30 21:57:31 +0100

    count starst correctiom

--- tests/countstars/main.go
+++ tests/countstars/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	table := []int{1, 2, 9, -258, 0, 48, 94, 41,-56}
+	table := []int{1, 2, 9, -258, 0, 100, 48, 94, 41, -56, 520}
 	for _, num := range table {
 		challenge.Function("CountStars", student.CountStars, solutions.CountStars, num)
 	}
