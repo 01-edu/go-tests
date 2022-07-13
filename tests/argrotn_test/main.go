827834ab8392c261a8fb5e91e0b3112c9f23ef2c
hamza <hamzaelkhatri@gmail.com>
2022-07-13 13:26:58 +0100

    Fix format

--- tests/argrotn_test/main.go
+++ tests/argrotn_test/main.go
@@ -7,13 +7,13 @@ import (
 func main() {
 	args := [][]string{
 		{""},
-		{"1","One", "ring!"},
-		{"2","testing spaces and #!*"},
-		{"3","more", "than", "three", "arguments"},
-		{"10","Upper anD LoWer cAsE"},
-		{"0","Hello", "World","everyOne"},
-		{"-1","Hello", "World","everyOne"},
-		{"4","Hello", "World","everyOne"},
+		{"1", "One", "ring!"},
+		{"2", "testing spaces and #!*"},
+		{"3", "more", "than", "three", "arguments"},
+		{"10", "Upper anD LoWer cAsE"},
+		{"0", "Hello", "World", "everyOne"},
+		{"-1", "Hello", "World", "everyOne"},
+		{"4", "Hello", "World", "everyOne"},
 	}
 
 	for _, v := range args {
