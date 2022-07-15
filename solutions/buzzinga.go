b5b2e66f70dd2259648e0e18acc43de42b13217e
zainabdnaya <diyanazizo13@gmail.com>
2022-07-15 16:56:09 +0100

    corrected

--- solutions/buzzinga.go
+++ solutions/buzzinga.go
@@ -7,6 +7,9 @@ func BuzZinga(n int32) {
 	if n == 0 {
 		fmt.Println("Buzzinga")
 	}
+	if n < 0 {
+		fmt.Print("*")
+	}
 	for i = 1; i <= n; i++ {
 		if i%3 == 0 && i%5 == 0 {
 			fmt.Println("Buzzinga")
