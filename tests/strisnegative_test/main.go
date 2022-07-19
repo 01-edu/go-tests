9a04446939a85018ac833c61ca5c1562f6511237
zainabdnaya <diyanazizo13@gmail.com>
2022-07-19 10:25:44 +0100

    corrected

--- tests/strisnegative_test/main.go
+++ tests/strisnegative_test/main.go
@@ -13,12 +13,18 @@ func main() {
 		"0",
 		"-551",
 		"+551",
+		"585 41 -9",
 		"hekiii",
 		"sda",
 		"2147483647",
 		"5898",
 		"697abc8",
-		"-2147483648"}
+		"82-58",
+		"-2147483648",
+		" -585",
+		"10 15",
+		"975 ",
+	}
 	for _, arg := range table {
 		challenge.Function("StrisNegative", student.StrisNegative, solutions.StrisNegative, arg)
 	}
