a099d8c6dd3c97d5f9c7717fe84571c38500b5bf
zainab Dnaya <diyanazizo13@gmail.com>
2022-07-26 18:01:15 +0100

    Update unzipstring.go

--- solutions/unzipstring.go
+++ solutions/unzipstring.go
@@ -2,17 +2,28 @@ package solutions
 
 import "strconv"
 
+func check(a rune) bool {
+	if (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'){
+		return true
+	}
+	return false
+}
+
 func Unzipstring(str string) string {
 	var result string
-	if len(str) == 0 || len(str) < 2 {
+	if len(str) == 0 {
 		return "Invalid output"
 	}
 	num, err := strconv.Atoi(string(str[0]))
-	if err != nil || (str[1] >= '0' && str[1] <= '9') {
+	if err != nil {
 		return "Invalid output"
 	}
 	for i := 0; i < num; i++ {
-		result += string(str[1])
+		if check(rune(str[1])) == true {
+			result += string(str[1])
+		} else {
+			return "Invalid output"
+		}
 	}
 	if len(str) > 2 {
 		if Unzipstring(string(str[2:])) != "Invalid output" {
