8447f7ff62cb5e804dbd05746a197e58aa7da96d
zainab Dnaya <diyanazizo13@gmail.com>
2022-07-26 17:12:49 +0100

    Update main.go

--- tests/argsort_test/main.go
+++ tests/argsort_test/main.go
@@ -15,6 +15,17 @@ func main() {
 		{"123qa"},
 		{"	 HELLO!"},
 		{" kowz"},
+		{"a b c"},
+		{"   abc"},
+		{"    hello"},
+		{"hello    "},
+		{"sto  ry"},
+		{"  @\!\\n"},
+		{"9^_\`abc"},
+		{"	\n"},
+		{"\t\v\n"},
+		{"%Hey|}"},
+		{"123	$_$%d"},
 	}
 	for _, v := range args {
 		challenge.Program("argsort", v...)
