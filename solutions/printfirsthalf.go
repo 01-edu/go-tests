90c64f57af768f1102c9b2428e9b9fcc2c9b05c7
zainabdnaya <diyanazizo13@gmail.com>
2022-07-28 00:06:11 +0100

    feat: solution and test of Printfirsthalf

---
+++ solutions/printfirsthalf.go
@@ -0,0 +1,19 @@
+package solutions
+
+import "strings"
+
+func Printfirsthalf(str string) string {
+	if len(str) == 0 {
+		return "Invalid Output\n"
+	} else if len(str) == 1 {
+		return (str + "\n")
+	} else {
+		var res strings.Builder
+		i := 0
+		for i = 0; i <= int(len(str)/2); i++ {
+			res.WriteRune(rune(str[i]))
+		}
+		res.WriteRune(rune('\n'))
+		return res.String()
+	}
+}
