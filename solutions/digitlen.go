ea3df25d2621dfbe91d0d412b8f31c978d9e5dc4
hamza <hamzaelkhatri@gmail.com>
2022-07-28 11:43:44 +0100

    DEV-3198 add(test-solution):for digitlen

---
+++ solutions/digitlen.go
@@ -0,0 +1,16 @@
+package solutions
+
+func DigitLen(num, base int) int {
+	if num < 0 {
+		num = -num
+	}
+	if base < 2 {
+		return -1
+	}
+	res := 0
+	for num > 0 {
+		num /= base
+		res++
+	}
+	return res
+}
