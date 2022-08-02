d4f471e083a8e2514917ab127b6428e13c6a1c0d
hamza <hamzaelkhatri@gmail.com>
2022-08-02 23:53:44 +0100

    DEV-3384 fix(test): remove fmt library

--- tests/iscapitalize_test/main.go
+++ tests/iscapitalize_test/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	student "student"
 
 	"github.com/01-edu/go-tests/lib/challenge"
@@ -25,7 +24,6 @@ func main() {
 	}
 
 	for _, arg := range table {
-		fmt.Printf("[%s] =>  [%t]\n", arg, student.IsCapitalize(arg))
 		challenge.Function("IsCapitalize", student.IsCapitalize, solutions.IsCapitalize, arg)
 	}
 }
