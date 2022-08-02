535ad96be2d82eb261a64f7ab00eb1a06e430533
hamza <hamzaelkhatri@gmail.com>
2022-08-02 23:50:33 +0100

    DEV-3384 test(is_capitalze):add solution and & test

---
+++ tests/iscapitalize_test/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	student "student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/solutions"
+)
+
+func main() {
+	table := []string{
+		"Hello! €How are you?",
+		"a",
+		"z",
+		"!",
+		"Hello How Are 4you",
+		"What's this 4?",
+		"Whatsthis4",
+		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+		"131!",
+		"H3110 W0r1d!",
+		"",
+		" ",
+	}
+
+	for _, arg := range table {
+		fmt.Printf("[%s] =>  [%t]\n", arg, student.IsCapitalize(arg))
+		challenge.Function("IsCapitalize", student.IsCapitalize, solutions.IsCapitalize, arg)
+	}
+}
