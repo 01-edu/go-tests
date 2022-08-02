fb87f3e11f126e5f3cd6070d35266e28d4699bc6
hamza <hamzaelkhatri@gmail.com>
2022-08-02 23:50:33 +0100

    DEV-3384 test(is_capitalze):add solution and & test

---
+++ solutions/iscapitalize.go
@@ -0,0 +1,14 @@
+package solutions
+
+func IsCapitalize(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+
+	for i := 0; i < len(s); i++ {
+		if s[i] >= 'a' && s[i] <= 'z' && i != 0 && s[i-1] == ' ' {
+			return false
+		}
+	}
+	return !(s[0] >= 'a' && s[0] <= 'z')
+}
