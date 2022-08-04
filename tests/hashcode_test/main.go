beceb73afe4a5b61f48f3c2b5cbfb857bcf88c52
zainabdnaya <diyanazizo13@gmail.com>
2022-08-05 00:49:46 +0100

    Add solutions and tests for Hashcode

---
+++ tests/hashcode_test/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	student "student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/solutions"
+)
+
+func main() {
+	table := []string{"Z", "Hi!", "BB198365", "Tanjirō Kamado", "sabito", "14 Avril 1912", "zyx987bca", "		pool-2020", "965truma747", " Mercedes-AMG GT"}
+	for _, s := range table {
+		challenge.Function("HashCode", student.HashCode, solutions.HashCode, s)
+	}
+}
