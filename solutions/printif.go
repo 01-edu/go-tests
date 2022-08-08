a41b063e66a5becafa052347daa48500301ef3d6
zainabdnaya <diyanazizo13@gmail.com>
2022-08-08 19:11:44 +0100

    tests(Printif) : add subject plus solution

--- solutions/printif.go
+++ solutions/printif.go
@@ -3,7 +3,6 @@ package solutions
 func PrintIf(arg string) string {
 	if len(arg) == 0 || len(arg) < 3 {
 		return "Invalid Output\n"
-	} else {
-		return "G\n"
 	}
+	return "G\n"
 }
--- tests/printif_test/main.go
+++ tests/printif_test/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	student "student"
-
 	"github.com/01-edu/go-tests/lib/challenge"
 	"github.com/01-edu/go-tests/solutions"
+	student "student"
 )
 
 func main() {
