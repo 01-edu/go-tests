ea1ed8c7910137dd3eaed7b1b874f12e50892240
zainabdnaya <diyanazizo13@gmail.com>
2022-08-10 10:48:30 +0100

    docs(thirdtimeischarm): Subject thirdtimeischarm

---
+++ tests/thirdTimeIsACharm_test/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	student "student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/solutions"
+)
+
+func main() {
+	table := []string{"1234556789", "QKplq%QSw", "", "Kimetsu no Yaiba", "Z", "email123@live.fr", "8595485-52", "-552", "w58tw7474abc", "fifa world cup `2022`"}
+	for _, s := range table {
+		challenge.Function("ThirdTimeIsACharm", student.ThirdTimeIsACharm, solutions.ThirdTimeIsACharm, s)
+	}
+}
