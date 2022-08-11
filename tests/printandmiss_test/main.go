8d5d7154ee9f8402ab96eee91d1c1b7bff2c2720
zainabdnaya <diyanazizo13@gmail.com>
2022-08-11 17:51:30 +0100

    test(printandmiss): solutions and test

--- tests/printandmiss_test/main.go
+++ tests/printandmiss_test/main.go
@@ -8,13 +8,23 @@ import (
 )
 
 func main() {
-	table := []string{"1234556789", "e 5£ @ 8* 7 =56 ;", "QKplq%QSw", "", "hello \\! n4ght cr3a8ure7 ", "Kimetsu no Yaiba", "8595485-52", "abcdefghijklmnopqrstuvwyz", "w58tw7474abc", "Po65 4o"}
-	_itt := []int{1, 2, 3, 4, 5, 0, -25, 8, -1, 8}
-	i := 0
-	j := 0
-	for i < len(table) && j < len(_itt) {
-		challenge.Function("PrintAndMiss", student.PrintAndMiss, solutions.PrintAndMiss, table[i], _itt[j])
-		i++
-		j++
+	structs := []struct {
+		str string
+		itt int
+	}{
+		{"123456789", 1},
+		{"e 5£ @ 8* 7 =56 ;", 2},
+		{"QKplq%QSw", 3},
+		{"", 4},
+		{"hello \\! n4ght cr3a8ure7 ", 5},
+		{"Kimetsu no Yaiba", 6},
+		{"8595485-52", 7},
+		{"abcdefghijklmnopqrstuvwyz", 8},
+		{"w58tw7474abc", 9},
+		{"Po65 4o", 10},
+	}
+
+	for _, v := range structs {
+		challenge.Function("PrintAndMiss", student.PrintAndMiss, solutions.PrintAndMiss, v.str, v.itt)
 	}
 }
