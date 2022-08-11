cedc2ba1d240a978a522f87b057c26f6e653f485
zainabdnaya <diyanazizo13@gmail.com>
2022-08-11 17:41:59 +0100

    test(get-ascii): solutions and test

---
+++ tests/getascii_test/main.go
@@ -0,0 +1,36 @@
+package main
+
+ import (
+ 	"github.com/01-edu/go-tests/lib/challenge"
+ )
+
+ func main() {
+ 	args := [][]string{
+ 		{"W"},
+ 		{""},
+ 		{"H", "world"},
+ 		{"Hello World"},
+ 		{"A"},
+ 		{"B"},
+ 		{"C"},
+ 		{"&"},
+ 		{"20"},
+ 		{"100"},
+ 		{" "},
+ 		{"4"}, {"E"},
+ 		{"	"},
+ 		{"13 233 4 23"},
+ 		{"1"},
+ 		{"7"},
+ 		{"8"},
+ 		{"£"},
+ 		{"@"},
+ 		{"$"},
+ 		{"~"},
+ 		{"0"},
+ 		{"*"},
+ 	}
+ 	for _, s := range args {
+ 		challenge.Program("getascii", s...)
+ 	}
+ }
