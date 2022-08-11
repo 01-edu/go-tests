892d8778c739d1df679eeed0b1fbbe1292f32dd5
zainabdnaya <diyanazizo13@gmail.com>
2022-08-11 14:44:31 +0100

    test(not_decimal): solutions and test

---
+++ solutions/not_decimal.go
@@ -0,0 +1,26 @@
+package solutions
+
+import (
+	"math"
+	"strconv"
+)
+
+func Not_decimal(dec string) string {
+	j := -1
+	n := 0
+	if len(dec) == 0 {
+		return "\n"
+	}
+	for i := 0; i < len(dec); i++ {
+		if j == -1 && dec[i] == '.' {
+			j++
+		} else if j == 0 {
+			n++
+		}
+	}
+	s, err := strconv.ParseFloat(dec, 64)
+	if err == nil {
+		return strconv.Itoa(int(s*math.Pow(10, float64(n)))) + "\n"
+	}
+	return (dec + "\n")
+}
---
+++ tests/not_decimal_test/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/solutions"
+)
+
+func main() {
+	args := []string{"-19.525856", "00.02", "56s44", "", "415.458", "1.6", "165", "502.3254", "51.3+95.9", "-5.0f00d00", "-53.124"}
+
+	for i := 0; i < len(args); i++ {
+		challenge.Function("Not_decimal", student.Not_decimal, solutions.Not_decimal, args[i])
+	}
+}
