c36af979493eda72ba919053ca31e5f524a9e2dc
Tiago Collot <collot.tiago1@gmail.com>
2022-08-24 18:03:10 +0100

    feat: add new go test for jumpover exercise

---
+++ tests/jumpover_test/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	student "student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests-training/solutions"
+)
+
+func main() {
+	table := []string{"1010101010", "plq%QSw", "", "Life Is Great", "Z", "johndoe@imposssible.io", "8595485-52", "-552", "w58tw7474abc", "Mine Craft"}
+	for _, s := range table {
+		challenge.Function("JumpOver", student.JumpOver, solutions.JumpOver, s)
+	}
+}
