fffea0667ee96a186307d7ac04add1e9043461b8
Tiago Collot <collot.tiago1@gmail.com>
2022-08-30 12:05:48 +0100

    feat(piscine-go): add edge case

--- tests/loafofbread_test/main.go
+++ tests/loafofbread_test/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+
 	student "student"
 
 	"github.com/01-edu/go-tests/lib/challenge"
@@ -8,8 +9,26 @@ import (
 )
 
 func main() {
-	table := []string{"Loaf of bread", "amazing loaf of bread", "bread crumbles", "", "bread slice", "bread"}
-	for _, s := range table {
+	table := []string{
+		"Loaf of bread",
+		"amazing loaf of bread",
+		"bread crumbles",
+		"",
+		"bread slice",
+		"bread",
+		"This is a loaf of bread",
+		"This is a loaf of brea",
+		" This is a loaf of bread",
+		" This is a loaf of bread ",
+		"This is a loaf of bre ",
+		"This     is a loaf  of bread",
+		"This     is a  loaf of bre ",
+		"This is a loaf of bre  ",
+		"This is a loaf of br  ",
+		"      This     is a l o a f     of   br    ead",
+		"                       "}
+
+		for _, s := range table {
 		challenge.Function("LoafOfBread", student.LoafOfBread, solutions.LoafOfBread, s)
 	}
 }
