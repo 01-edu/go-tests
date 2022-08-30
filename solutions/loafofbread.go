66ac48901ded8f8a852c1f7ec1d17b5ef0b104af
Tiago Collot <collot.tiago1@gmail.com>
2022-08-30 12:03:21 +0100

    fix(piscine-go): add if statement

--- solutions/loafofbread.go
+++ solutions/loafofbread.go
@@ -9,17 +9,25 @@ func LoafOfBread(str string) string {
 	if len(str) < 5 {
 		return "Invalid Output\n"
 	}
-	s := strings.ReplaceAll(str, " ", "")
 	var _str strings.Builder
 	j := 0
-	for i := 0; i < len(s); i++ {
-		if j == 5 {
-			_str.WriteRune(rune(' '))
-			j = 0
-		} else {
-			_str.WriteRune(rune(s[i]))
-			j++
+	for i := 0; i < len(str); i++ {
+		if j < 5 && rune(str[i]) == ' ' {
+			continue
 		}
+		if j == 5 {
+			if i != len(str)-1 && str[i+1] == ' ' {
+				continue
+			}
+			if i == len(str)-1 {
+				break
+			}
+			_str.WriteRune(' ')
+			j = 0
+			continue
+		}
+		_str.WriteRune(rune(str[i]))
+		j++
 	}
 	_str.WriteRune(rune('\n'))
 	return (_str.String())
