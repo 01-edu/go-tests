737cdb6f3e6e565610290fd98be3e1aad665a2a1
Tiago Collot <collot.tiago1@gmail.com>
2022-09-01 19:19:31 +0100

    feat(piscine-go): add solution for new go exercise shoppingsummarycounter

---
+++ solutions/shoppingsummarycounter.go
@@ -0,0 +1,19 @@
+package solutions
+
+import (
+    "fmt"
+    "strings"
+)
+func ShoppingSummaryCounter(str string) map[string]int {
+    wordList := strings.Split(str, " ")
+    counts := make(map[string]int)
+    for _, word := range wordList {
+        _, ok := counts[word]
+        if ok {
+            counts[word] += 1
+        } else {
+            counts[word] = 1
+        }
+    }
+    return counts
+}
