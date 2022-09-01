306b30efe6868e8d82b3700da3c24dfcf1fb4a71
Tiago Collot <collot.tiago1@gmail.com>
2022-09-01 19:24:11 +0100

    feat(piscine-go): add test for new go exercise shoppingsummarycounter

---
+++ tests/shoppingsummarycounter_test/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	student "student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests-training/solutions"
+)
+
+func main() {
+	table := []string{"Burger Burger Water Coffee Water Chips Carrot", "Burger Burger Water Coffe    e Water Chips Carrot", "Burger Burger Water Coffe    e Water Chips Carrot", "32312&%", " ", "", "#$sds "}
+
+	for _, s := range table {
+		challenge.Function("ShoppingSummaryCounter", student.ShoppingSummaryCounter, solutions.ShoppingSummaryCounter, s)
+	}
+}
