2108e663b15b415749280d5e03750c4c5b778057
Tiago Collot <collot.tiago1@gmail.com>
2022-09-05 15:44:33 +0100

    feat(piscine-go): add test for new go exercise podiumposition

---
+++ tests/podiumposition_test/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	student "student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests-training/solutions"
+)
+
+func main() {
+	position := [][][]string {
+	{{"4th Place"}, {"3rd Place"}, {"2nd Place"}, {"1st Place"}},
+	{{"4th Place "}, {"3rd   Place"}, {"2nd Place  "}, {" 1st Place"}},
+	{{""}, {"+55","*@Â£%D", "edr8927", "		| cat -e"}, {" "}},
+	{{"741", "852","4","58","87","03","-96"},{"kd","jq","hel$lo","a","B","9W"}},
+}
+	for _, s := range position {
+		challenge.Function("PodiumPosition", student.PodiumPosition, solutions.PodiumPosition, s)
+	}
+}
