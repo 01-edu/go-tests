96123497d211ae476df3b1a4139d3e40fcb49aea
Tiago Collot <collot.tiago1@gmail.com>
2022-09-06 16:33:33 +0100

    feat(piscine-go): add test/solution for new go exercise descendcomb

---
+++ solutions/descendcomb.go
@@ -0,0 +1,28 @@
+package solutions
+
+import "github.com/01-edu/z01"
+
+func printInteger(a int) {
+		secondDigits := (a % 10)
+	firstDigits := (a / 10)
+
+	z01.PrintRune(rune(firstDigits) + 48)
+	z01.PrintRune(rune(secondDigits) + 48)
+}
+
+func DescendComb() {
+    for i := 99; i >= 0; i-- {
+        for j := i - 1; j >= 0; j-- {
+            printInteger(i)
+            z01.PrintRune(' ')
+            printInteger(j)
+            if i == 01 && j == 00 {
+							break
+							z01.PrintRune('\n')
+            }
+            z01.PrintRune(',')
+            z01.PrintRune(' ')
+
+        }
+    }
+}
