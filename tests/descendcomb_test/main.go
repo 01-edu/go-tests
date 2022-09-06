ab58f7b1dad5b1726c0995264edfbdd71f88d0d8
Tiago Collot <collot.tiago1@gmail.com>
2022-09-06 16:33:33 +0100

    feat(piscine-go): add test/solution for new go exercise descendcomb

---
+++ tests/descendcomb_test/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	student "student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests-training/solutions"
+
+)
+
+func main() {
+	challenge.Function("DescendComb", student.DescendComb, solutions.DescendComb)
+}
