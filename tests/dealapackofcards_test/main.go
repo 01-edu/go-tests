eb5bb97ac503f7e091985477f73ffc75fefc2c86
Tiago Collot <collot.tiago1@gmail.com>
2022-09-08 18:04:59 +0100

    feat(piscine-go): add solution/test for new go exercise dealapackofcards

---
+++ tests/dealapackofcards_test/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	student "student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/solutions"
+)
+
+func main() {
+	table := [][]int{{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+									 {1, 2, 3, 4, 5, 17, 7  , 8, 9, 10   , 11 , 12},
+									 {1, 2, 3, 4, 5, 6, 7  , 8, 9, 19   , 16 , 12}}
+
+	for _, s := range table {
+		challenge.Function("DealAPackOfCards", student.DealAPackOfCards, solutions.DealAPackOfCards, s)
+	}
+}
