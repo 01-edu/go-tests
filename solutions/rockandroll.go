3bf28ff4e1ab1cab1c164a251727670a9b43c3a4
Tiago Collot <collot.tiago1@gmail.com>
2022-09-08 12:07:32 +0100

    style(rockandroll): remove extra space

--- solutions/rockandroll.go
+++ solutions/rockandroll.go
@@ -10,7 +10,7 @@ func RockAndRoll (n int) string {
   } else if (n % 3 == 0)  && !(n % 2 == 0) {
      return "roll\n"
 	} else if !(n < 0) || !((n%2 == 0) && (n%3 == 0)) {
-		 return "error: number is negative or non divisible "
+		 return "error: number is negative or non divisible"
 	}
 	 return strconv.Itoa(n)
 }
