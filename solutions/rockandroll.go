ed228acd7e38a3d214422a1ee4f59a2eece2f9f1
Tiago Collot <collot.tiago1@gmail.com>
2022-09-08 11:55:44 +0100

    fix(rockandroll): add missing else if statement

--- solutions/rockandroll.go
+++ solutions/rockandroll.go
@@ -4,11 +4,13 @@ import "strconv"
 
 func RockAndRoll (n int) string {
   if (n % 2 == 0) && (n % 3 == 0) {
-        return "rock and roll\n"
+    return "rock and roll\n"
   } else if (n % 2 == 0) && !(n % 3 == 0) {
-        return "rock\n"
+     return "rock\n"
   } else if (n % 3 == 0)  && !(n % 2 == 0) {
-        return "roll\n"
+     return "roll\n"
+	} else if !(n < 0) || !((n%2 == 0) && (n%3 == 0)) {
+		 return "error: number is negative or non divisible "
 	}
 	 return strconv.Itoa(n)
 }
