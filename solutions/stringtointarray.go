12f5ee0ff3f482e2851b2b09a2a2860eb36886a7
Tiago Collot <collot.tiago1@gmail.com>
2022-09-19 14:38:42 +0100

    feat(piscine-go): add solution for new go exercise stringtointarray

---
+++ solutions/stringtointarray.go
@@ -0,0 +1,12 @@
+package solutions
+
+func StringToIntArray(str string) []int {
+	runes := []rune(str)
+
+	var result []int
+
+	for i := 0; i < len(runes); i++ {
+		result = append(result, int(runes[i]))
+	}
+	return result
+}
