e6c145d56a5660c5368ef0532349f6e3c6ecd058
Tiago Collot <collot.tiago1@gmail.com>
2022-09-21 10:42:07 +0100

    feat(piscine-go): add solution/test for new go exercise fooddeliverytime

---
+++ tests/fooddeliverytime_test/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  student "student"
+
+  "github.com/01-edu/go-tests/lib/challenge"
+  "github.com/01-edu/go-tests/solutions"
+)
+
+func main() {
+  table := []string{"burger", "chips", "nuggets", "pizza", "pepper", "  burger", "chi ps", "nuggets  "}
+   for _, s := range table {
+    challenge.Function("FoodDeliveryTime", student.FoodDeliveryTime, solutions.FoodDeliveryTime, s)
+   }
+}
