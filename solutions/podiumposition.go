6b71d55e2be82cbd203952135fa5f081f19e7089
Tiago Collot <collot.tiago1@gmail.com>
2022-09-27 11:12:29 +0100

    style(podiumposition): correct space in the wrong place

--- solutions/podiumposition.go
+++ solutions/podiumposition.go
@@ -1,6 +1,6 @@
 package solutions
 
-func PodiumPosition(podium[][] string) [][]string {
+func PodiumPosition(podium [][]string) [][]string {
 	for i, j := 0, len(podium) - 1; i < j; i, j = i + 1, j - 1 {
         podium[i], podium[j] = podium[j], podium[i]
     }
