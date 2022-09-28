6190998e903bba593f787fe9156f88ea3b850193
Tiago Collot <collot.tiago1@gmail.com>
2022-09-28 13:09:04 +0100

    style(reversemenuindex): fix identation

--- tests/reversemenuindex_test/main.go
+++ tests/reversemenuindex_test/main.go
@@ -7,21 +7,21 @@ import (
 	"github.com/01-edu/go-tests/solutions"
 )
 
-
 func main() {
-	array := [][]string{
-	{"desserts", "mains", "drinks", "starters"},
-	{"des  rts", "mains ", "  drinks", "starters  "},
-	{"", "desrts", "mins", "drins", "strters"},
-	{" ", "des serts", "ma ins", "drink s", "st arters"},
-	{"desserts", "mai  ns", "dr   inks", "st3arters"},
-	{"desserts", "mains", "drinks", "starters"},
-	{""},
-	{"+55","*@Â£%D", "edr8927", "		| cat -e"},
-	{"741", "852","4","58","87","03","-96"},
-	{"kd","jq","hel$lo","a","B","9W"},
-}
-	for _, s := range array {
+	slice := [][]string{
+		{"desserts", "mains", "drinks", "starters"},
+		{"des  rts", "mains ", "  drinks", "starters  "},
+		{"", "desrts", "mins", "drins", "strters"},
+		{" ", "des serts", "ma ins", "drink s", "st arters"},
+		{"desserts", "mai  ns", "dr   inks", "st3arters"},
+		{"desserts", "mains", "drinks", "starters"},
+		{""},
+		{"+55", "*@Â£%D", "edr8927", "		| cat -e"},
+		{"741", "852", "4", "58", "87", "03", "-96"},
+		{"kd", "jq", "hel$lo", "a", "B", "9W"},
+	}
+
+	for _, s := range slice {
 		challenge.Function("ReverseMenuIndex", student.ReverseMenuIndex, solutions.ReverseMenuIndex, s)
 	}
 }
