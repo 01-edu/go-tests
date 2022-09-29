b4da3f4bcace17d66b45b230cfbba665df7789f8
Tiago Collot <collot.tiago1@gmail.com>
2022-09-29 16:04:12 +0100

    style(stringtointslice): fix indentation

--- tests/stringtointslice_test/main.go
+++ tests/stringtointslice_test/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	table := []string{"hello THERE" ,"Converted this string into an int", "14/%37", "a b c d e f g h i j k l m n o p q r s t u v w x y z", "a b c d e fghijk l m n o p q r s t u v w x y z ", "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z", "heLLo There", "  121314", "qwçiodhqwd", " ", ""}
+	table := []string{"hello THERE", "Converted this string into an int", "14/%37", "a b c d e f g h i j k l m n o p q r s t u v w x y z", "a b c d e fghijk l m n o p q r s t u v w x y z ", "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z", "heLLo There", "  121314", "qwçiodhqwd", " ", ""}
 
 	for _, s := range table {
 		challenge.Function("StringToIntSlice", student.StringToIntSlice, solutions.StringToIntSlice, s)
