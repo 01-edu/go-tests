8b459982b1f7bb85fdd955ef04796ca7d1983d4b
Tiago Collot <collot.tiago1@gmail.com>
2022-10-03 16:59:21 +0100

    test(dealapackofcards): fix indentation, refactor code

--- tests/dealapackofcards_test/main.go
+++ tests/dealapackofcards_test/main.go
@@ -9,8 +9,8 @@ import (
 
 func main() {
 	table := [][]int{{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
-									 {1, 2, 3, 4, 5, 17, 7  , 8, 9, 10   , 11 , 12},
-									 {1, 2, 3, 4, 5, 6, 7  , 8, 9, 19   , 16 , 12}}
+		{11, 2, 14, 4, 5, 17, 7, 8, 9, 10, 11, 12},
+		{1, 2, 3, 1, 5, 5, 7, 8, 9, 19, 16, 12}}
 
 	for _, s := range table {
 		challenge.Function("DealAPackOfCards", student.DealAPackOfCards, solutions.DealAPackOfCards, s)
