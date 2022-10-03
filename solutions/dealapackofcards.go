deee5186061416cd6afe4ef9073b9e4c913a33f0
Tiago Collot <collot.tiago1@gmail.com>
2022-10-03 16:42:26 +0100

    refactor(solutions): refactor code, fix indentation

--- solutions/dealapackofcards.go
+++ solutions/dealapackofcards.go
@@ -2,18 +2,18 @@ package solutions
 
 import "fmt"
 
-func DealAPackOfCards (deck []int) {
-	for _,v := range deck {
-		if (v == 1 && v < 3)  {
+func DealAPackOfCards(deck []int) {
+	for _, v := range deck {
+		if v == 1 {
 			fmt.Printf("Player 1: %v, %v, %v", deck[0], deck[1], deck[2])
 			fmt.Println()
-		} else if (v == 3 && v < 6 ){
+		} else if v == 3 {
 			fmt.Printf("Player 2: %v, %v, %v", deck[3], deck[4], deck[5])
 			fmt.Println()
-		} else if ( v == 6 && v < 9){
+		} else if v == 6 {
 			fmt.Printf("Player 3: %v, %v, %v", deck[6], deck[7], deck[8])
 			fmt.Println()
-		} else if (v == 9 && v <= 11){
+		} else if v == 9 {
 			fmt.Printf("Player 4: %v, %v, %v", deck[9], deck[10], deck[11])
 			fmt.Println()
 		}
