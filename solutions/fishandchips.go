c76c44db1c925d989025830bfa53a5d48c733b1a
Tiago Collot <collot.tiago1@gmail.com>
2022-10-06 18:10:18 +0100

    style(fishandships): format solution indentation

--- solutions/fishandchips.go
+++ solutions/fishandchips.go
@@ -2,15 +2,15 @@ package solutions
 
 import "strconv"
 
-func FishAndChips (n int) string {
-  if (n % 2 == 0) && (n % 3 == 0) {
-    return "fish and chips\n"
-  } else if (n % 2 == 0) && !(n % 3 == 0) {
-     return "fish\n"
-  } else if (n % 3 == 0)  && !(n % 2 == 0) {
-     return "chips\n"
+func FishAndChips(n int) string {
+	if (n%2 == 0) && (n%3 == 0) {
+		return "fish and chips\n"
+	} else if (n%2 == 0) && !(n%3 == 0) {
+		return "fish\n"
+	} else if (n%3 == 0) && !(n%2 == 0) {
+		return "chips\n"
 	} else if !(n < 0) || !((n%2 == 0) && (n%3 == 0)) {
-		 return "error: number is negative or non divisible"
+		return "error: number is negative or non divisible"
 	}
-	 return strconv.Itoa(n)
+	return strconv.Itoa(n)
 }
