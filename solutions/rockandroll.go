694e5c60666d60c8ce1c893e3f3724a4591f3c3b
Tiago Collot <collot.tiago1@gmail.com>
2022-10-13 15:42:18 +0100

    style(rockandroll): format solution indentation

--- solutions/rockandroll.go
+++ solutions/rockandroll.go
@@ -2,18 +2,18 @@ package solutions
 
 import "strconv"
 
-func RockAndRoll (n int) string {
-  if (n % 2 == 0) && (n % 3 == 0) {
-   return "rock and roll\n"
-  }
-  if (n % 2 == 0) && !(n % 3 == 0) {
-   return "rock\n"
-  }
-  if (n % 3 == 0)  && !(n % 2 == 0) {
-   return "roll\n"
-  }
-  if !(n < 0) || !((n%2 == 0) && (n%3 == 0)) {
-   return "error: number is negative or non divisible"
-  }
-  return strconv.Itoa(n)
+func RockAndRoll(n int) string {
+	if (n%2 == 0) && (n%3 == 0) {
+		return "rock and roll\n"
+	}
+	if (n%2 == 0) && !(n%3 == 0) {
+		return "rock\n"
+	}
+	if (n%3 == 0) && !(n%2 == 0) {
+		return "roll\n"
+	}
+	if !(n < 0) || !((n%2 == 0) && (n%3 == 0)) {
+		return "error: number is negative or non divisible"
+	}
+	return strconv.Itoa(n)
 }
