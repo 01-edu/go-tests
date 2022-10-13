dd401bc070d2283750bfb9dbe39defab639841b5
Tiago Collot <collot.tiago1@gmail.com>
2022-10-13 18:07:56 +0100

    test(go-exam): rename folder, add missing import student

--- tests/notdecimal_test/main.go
+++ tests/notdecimal_test/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"student"
+	student "student"
 
 	"github.com/01-edu/go-tests/lib/challenge"
 	"github.com/01-edu/go-tests/solutions"
@@ -11,6 +11,6 @@ func main() {
 	args := []string{"-19.525856", "00.02", "56s44", "", "415.458", "1.6", "165", "502.3254", "51.3+95.9", "-5.0f00d00", "-53.124"}
 
 	for i := 0; i < len(args); i++ {
-		challenge.Function("Not_decimal", student.Not_decimal, solutions.Not_decimal, args[i])
+		challenge.Function("NotDecimal", student.NotDecimal, solutions.NotDecimal, args[i])
 	}
 }
