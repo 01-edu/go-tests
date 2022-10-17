577e9df8bee3440555e83ff20af7493e877df8f9
Tiago Collot <collot.tiago1@gmail.com>
2022-10-17 14:18:36 +0100

    style(shoppingsummarycouter): fix solution indentation

--- solutions/shoppingsummarycounter.go
+++ solutions/shoppingsummarycounter.go
@@ -3,15 +3,15 @@ package solutions
 import "strings"
 
 func ShoppingSummaryCounter(str string) map[string]int {
-    wordList := strings.Split(str, " ")
-    counts := make(map[string]int)
-    for _, word := range wordList {
-        _, ok := counts[word]
-        if ok {
-            counts[word] += 1
-        } else {
-            counts[word] = 1
-        }
-    }
-    return counts
+	wordList := strings.Split(str, " ")
+	counts := make(map[string]int)
+	for _, word := range wordList {
+		_, ok := counts[word]
+		if ok {
+			counts[word] += 1
+		} else {
+			counts[word] = 1
+		}
+	}
+	return counts
 }
