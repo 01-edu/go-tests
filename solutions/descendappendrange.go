def37e10256080da81afc4b9bba837a106dfc8b0
Tiago Collot <collot.tiago1@gmail.com>
2022-10-17 15:09:26 +0100

    fix(descendappendrange): change the return value from 'nil' to 'empty slice

--- solutions/descendappendrange.go
+++ solutions/descendappendrange.go
@@ -3,7 +3,7 @@ package solutions
 func DescendAppendRange(max, min int) []int {
 	answer := []int{}
 	if max <= min {
-		return nil
+		return []int{}
 	} else {
 		for i := max ; i > min; i-- {
 			answer = append(answer, i)
