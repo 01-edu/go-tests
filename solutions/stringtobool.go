3f5d658c9f8a29618388cfa71071338979f91576
Tiago Collot <collot.tiago1@gmail.com>
2022-10-19 11:28:45 +0100

    fix(stringtobool): fix solution/test
    - fix solution bug
    - add more test edge cases

--- solutions/stringtobool.go
+++ solutions/stringtobool.go
@@ -1,7 +1,7 @@
 package solutions
 
 func StringToBool(s string) bool {
-	if s == "true" || s == "TRUE" || s == "T" || s == "t" {
+	if s == "True" || s == "T" || s == "t" {
 		return true
 	}
 	return false
