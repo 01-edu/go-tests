da673aa52902e10765476b32d538cbf568233841
jotapero <j.pd19@hotmail.com>
2022-10-19 15:27:22 +0100

    docs(getAscii): fix test/solution files
    - fix indentation

--- solutions/getascii/main.go
+++ solutions/getascii/main.go
@@ -1,13 +1,13 @@
 package main
 
- import (
- 	"fmt"
- 	"os"
- )
+import (
+	"fmt"
+	"os"
+)
 
- func main() {
- 	if len(os.Args) == 2 && len(os.Args[1]) == 1 {
- 		fmt.Println(int(os.Args[1][0]))
- 	}
-  fmt.Print("\n")
- }
+func main() {
+	if len(os.Args) == 2 && len(os.Args[1]) == 1 && int(os.Args[1][0]) >= 0 && int(os.Args[1][0]) <= 127 {
+		fmt.Println(int(os.Args[1][0]))
+	}
+	fmt.Print("\n")
+}
