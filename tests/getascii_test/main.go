007c95df60ff3a8806234ac9d659ccb36ff07b82
jotapero <j.pd19@hotmail.com>
2022-10-19 15:27:22 +0100

    docs(getAscii): fix test/solution files
    - fix indentation

--- tests/getascii_test/main.go
+++ tests/getascii_test/main.go
@@ -1,39 +1,40 @@
 package main
 
- import (
- 	"github.com/01-edu/go-tests/lib/challenge"
- )
+import (
+	"github.com/01-edu/go-tests/lib/challenge"
+)
 
- func main() {
- 	args := [][]string{
- 		{"W"},
- 		{""},
- 		{"H", "world"},
- 		{"Hello World"},
- 		{"A"},
- 		{"B"},
- 		{"C"},
- 		{"&"},
- 		{"20"},
- 		{"100"},
- 		{" "},
- 		{"4"}, {"E"},
- 		{"	"},
- 		{"13 233 4 23"},
- 		{"1"},
- 		{"7"},
- 		{"8"},
- 		{"£"},
- 		{"@"},
- 		{"$"},
- 		{"~"},
- 		{"0"},
- 		{"*"},
-   {"本"},
-   {"人"},
-   {"的"},
- 	}
- 	for _, s := range args {
- 		challenge.Program("getascii", s...)
- 	}
- }
+func main() {
+	args := [][]string{
+		{"W"},
+		{""},
+		{"H", "world"},
+		{"Hello World"},
+		{"A"},
+		{"B"},
+		{"C"},
+		{"&"},
+		{"20"},
+		{"100"},
+		{" "},
+		{"4"},
+		{"E"},
+		{"	"},
+		{"13 233 4 23"},
+		{"1"},
+		{"7"},
+		{"8"},
+		{"£"},
+		{"@"},
+		{"$"},
+		{"~"},
+		{"0"},
+		{"*"},
+		{"本"},
+		{"人"},
+		{"的"},
+	}
+	for _, s := range args {
+		challenge.Program("getascii", s...)
+	}
+}
