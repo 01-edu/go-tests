342f91f761a66f86a78cd24f64cbefbc4a82e5bf
Tiago Collot <collot.tiago1@gmail.com>
2022-10-20 16:59:18 +0100

    fix(fishandchips): fix function code

--- solutions/fishandchips.go
+++ solutions/fishandchips.go
@@ -1,16 +1,18 @@
 package solutions
 
-import "strconv"
-
 func FishAndChips(n int) string {
+	if n < 0 {
+		return "error: number is negative"
+	}
+
 	if (n%2 == 0) && (n%3 == 0) {
 		return "fish and chips\n"
-	} else if (n%2 == 0) && !(n%3 == 0) {
-		return "fish\n"
-	} else if (n%3 == 0) && !(n%2 == 0) {
-		return "chips\n"
-	} else if !(n < 0) || !((n%2 == 0) && (n%3 == 0)) {
-		return "error: number is negative or non divisible"
 	}
-	return strconv.Itoa(n)
+	if n%2 == 0 {
+		return "fish\n"
+	}
+	if n%3 == 0 {
+		return "chips\n"
+	}
+	return "error: non divisible"
 }
