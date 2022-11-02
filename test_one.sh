15d308cfaac850d51a37115f0bdc69a46cab30c4
Tiago Collot <collot.tiago1@gmail.com>
2022-11-02 15:07:07 +0000

    fix(test_all-test_one): fix script for macbook

--- test_one.sh
+++ test_one.sh
@@ -3,17 +3,23 @@
 IFS='
 '
 
-if [ -z "$1" ]
- then
-   echo "No folder supplied"
-   exit 1
+if [ -z "$1" ]; then
+    echo "No folder supplied"
+    exit 1
 else
     testFile="./tests/$1_test/main.go"
     if test -f "$testFile"; then
-
-    rm -rf /tmp/binaries/student/
-    go run $testFile
-    echo "$1 test executed!"
+        # Fix for mac and linux temp directories
+        if [ -z "$TMPDIR" ]; then
+            # Linux
+            rm -rf /tmp/binaries/student/
+        else
+            # Mac
+            rm -rf $TMPDIR/binaries/student/
+        fi
+        # End of fix
+        go run $testFile
+        echo "$1 test executed!"
     else
         echo "Error: $testFile does not exist."
     fi
