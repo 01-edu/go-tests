8a7eb9e68675c495256e9601300b7a805d4b867a
Tiago Collot <collot.tiago1@gmail.com>
2022-11-02 15:07:07 +0000

    fix(test_all-test_one): fix script for macbook

--- test_all.sh
+++ test_all.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+#set -euo pipefail
 IFS='
 '
 # clear the student folder go binary cache
-rm -rf /tmp/binaries/student/
+# Fix for mac and linux temp directories
+if [ -z "$TMPDIR" ]; then
+    # Linux
+    rm -rf /tmp/binaries/student/
+else
+    # Mac
+    rm -rf $TMPDIR/binaries/student/
+fi
+# End of fix
 
 for dir in ./tests/*; do
     echo "$dir executed"
