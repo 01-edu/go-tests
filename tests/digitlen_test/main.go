c24e9bbd3e6f2418c0056be4d68f9070ad6e492f
Tiago Collot <collot.tiago1@gmail.com>
2022-11-08 16:06:32 +0000

    feat(digitlen): add test/solution for new go exercise digitlen

---
+++ tests/digitlen_test/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	student "student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/solutions"
+)
+
+func main() {
+	table := [][]int{
+		{10, 2},
+		{10, 3},
+		{10, 4},
+		{10, 5},
+		{10, 6},
+		{100, -1},
+		{100, 10},
+		{-100, 10},
+		{-100, -1},
+		{-100, 2},
+		{-100, 15},
+		{-100, 8},
+		{-100, 16},
+	}
+
+	for _, arg := range table {
+		challenge.Function("DigitLen", student.DigitLen, solutions.DigitLen, arg[0], arg[1])
+	}
+}
