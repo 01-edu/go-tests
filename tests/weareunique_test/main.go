f7219445edefebe344c512b04bcb1c330429f9d0
jrosendo <jose@01talent.com>
2022-11-08 16:40:50 +0000

    fix(weareunique): fixed branch
    - removed unnecessary files

---
+++ tests/weareunique_test/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	student "student"
+
+	"github.com/01-edu/go-tests/lib/challenge"
+	"github.com/01-edu/go-tests/solutions"
+)
+
+func main() {
+	table := [][]string{
+		{"abc", "def"},
+		{"hello", "yoall"},
+		{"everyone", ""},
+		{"hello world", "fam"},
+		{"abc", "abc"},
+		{"", ""},
+		{"pomme", "pomme"},
+		{"+265", "265"},
+		{"123231", "123231"},
+		{"w^p@@j", "w^p@@j"},
+		{"26235e5", "4478q92"},
+		{"		", "		 "},
+		{"AB$%d.52", "eepqdl.52"},
+		{"", "eveRyone"},
+		{"_55w1se","55w1se"},
+	}
+	for _, arg := range table {
+		challenge.Function("Weareunique", student.Weareunique, solutions.Weareunique, arg[0], arg[1])
+	}
+}
