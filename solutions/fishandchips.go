36baa41893fa9659379e6850236025a37b705f8c
Tiago Collot <collot.tiago1@gmail.com>
2022-11-08 12:24:44 +0000

    fix(fishandchips): add missing newline '\n'

--- solutions/fishandchips.go
+++ solutions/fishandchips.go
@@ -2,7 +2,7 @@ package solutions
 
 func FishAndChips(n int) string {
 	if n < 0 {
-		return "error: number is negative"
+		return "error: number is negative\n"
 	}
 
 	if (n%2 == 0) && (n%3 == 0) {
@@ -14,5 +14,5 @@ func FishAndChips(n int) string {
 	if n%3 == 0 {
 		return "chips\n"
 	}
-	return "error: non divisible"
+	return "error: non divisible\n"
 }
