3674f4dda564294ef2ee6da4dfea4a34d69a67cf
Tiago Collot <collot.tiago1@gmail.com>
2022-11-08 15:17:03 +0000

    refactor(secondhalf): refactored solution code

--- solutions/secondhalf.go
+++ solutions/secondhalf.go
@@ -4,10 +4,10 @@ func SecondHalf(slice []int) []int {
 	var result []int
 	size := len(slice)
 
-	if len(slice)%2 == 0 {
-		size = len(slice) / 2
+	if size%2 == 0 {
+		size /= 2
 	} else {
-		size = len(slice) / 2
+		size /= 2
 	}
 	for i := size; i < len(slice); i++ {
 		result = append(result, slice[i])
