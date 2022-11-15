65c59c5fd403ccbfb64f96d34c10504094fd3252
jrosendo <jose@01talent.com>
2022-11-15 16:22:47 +0000

    fix(brainfuck): increased test robustness
    - added several combinations to increase complexity

--- tests/brainfuck_test/main.go
+++ tests/brainfuck_test/main.go
@@ -15,10 +15,27 @@ func main() {
 		"+++++[>++++[>++++H>+++++i<<-]>>>++\n<<<<-]>>--------.>+++++.>.",
 		"++++++++++[>++++++++++>++++++++++>++++++++++<<<-]>---.>--.>-.>++++++++++.",
 		"ld++++++++++++++++++++++++++++++++++++++++++++this+is++a++++comment++++++++++++++[>d+<-]>.+.+.>++++++++++.",
+
+		strings.Join(
+			[]string{
+				"++++++++++[>++++++++++>++++++++++>++++++++++<<<-]>---",
+				random.Str(".+", random.IntBetween(1, 25)),
+				random.Str(".a", random.IntBetween(1, 100)),
+				">--",
+				random.Str(".+", random.IntBetween(1, 20)),
+				random.Str(".a", random.IntBetween(1, 100)),
+				">-",
+				random.Str(".+", random.IntBetween(1, 20)),
+				random.Str(".a", random.IntBetween(1, 100)),
+				">++++++++++.",
+			},
+			"",
+		),
+
 		strings.Join(
 			[]string{
 				"ld++++++++++++++++++++++++++++++++++++++++++++this+is++a++++comment++++++++++++++[>d+<-]>.+",
-				random.Str(".+", random.IntBetween(1, 11)),
+				random.Str(".+", random.IntBetween(1, 10)),
 				".+.>++++++++++.",
 			},
 			"",
