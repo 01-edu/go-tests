348c62e2856417a4db3857a2e853eed7a18fd3db
Michele Sessa <mikysett@gmail.com>
2023-06-16 18:03:37 +0100

    test(thirdtimeisacharm): add edge case where erroneous imput was not detected

--- tests/thirdTimeIsACharm_test/main.go
+++ tests/thirdTimeIsACharm_test/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	table := []string{"1234556789", "QKplq%QSw", "", "Kimetsu no Yaiba", "Z", "email123@live.fr", "8595485-52", "-552", "w58tw7474abc", "fifa world cup `2022`"}
+	table := []string{"1234556789", "QKplq%QSw", "", "Kimetsu no Yaiba", "Z", "email123@live.fr", "8595485-52", "-552", "abc", "w58tw7474abc", "fifa world cup `2022`"}
 	for _, s := range table {
 		challenge.Function("ThirdTimeIsACharm", student.ThirdTimeIsACharm, solutions.ThirdTimeIsACharm, s)
 	}
