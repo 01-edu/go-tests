c5588f6e5ccc4fb91ee17002bc0faac3ff89e07f
Michele Sessa <mikysett@gmail.com>
2023-06-20 11:57:24 +0100

    test(quarterofayear): add test cases

--- tests/quarterofayear_test/main.go
+++ tests/quarterofayear_test/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	args := []int{0, 5, 2, 3, 3, -9, 9, 4, 7, 10, 12, 1, 11, 6, 8, -6}
+	args := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 0, 13, -5}
 
 	for _, arg := range args {
 		challenge.Function("QuarterOfAYear", student.QuarterOfAYear, solutions.QuarterOfAYear, arg)
