6e92fbaf4d2f7ba23eb54190b711301524e07d79
miguel <miguel-silva98@hotmail.com>
2023-06-21 16:42:41 +0100

    fix(revconcatalternate):fix the sol

--- solutions/revconcatalternate.go
+++ solutions/revconcatalternate.go
@@ -4,29 +4,18 @@ func RevConcatAlternate(slice1, slice2 []int) []int {
 	len1 := len(slice1)
 	len2 := len(slice2)
 	maxLen := len1
+	result := make([]int, 0, len1+len2)
+
 	if len2 > maxLen {
 		maxLen = len2
 	}
 
-	result := make([]int, 0, len1+len2)
-
-	if len1 == len2 {
-		for i := maxLen - 1; i >= 0; i-- {
-			if i < len1 {
-				result = append(result, slice1[i])
-			}
-			if i < len2 {
-				result = append(result, slice2[i])
-			}
+	for i := maxLen; i >= 0; i-- {
+		if i < len1 {
+			result = append(result, slice1[i])
 		}
-	} else {
-		for i := maxLen - 1; i >= 0; i-- {
-			if i < len1 {
-				result = append(result, slice1[i])
-			}
-			if i < len2 {
-				result = append(result, slice2[i])
-			}
+		if i < len2 {
+			result = append(result, slice2[i])
 		}
 	}
 
