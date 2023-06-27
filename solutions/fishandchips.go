9698316b448187deddfc602c9f49c7e721e8be58
Michele Sessa <mikysett@gmail.com>
2023-06-27 13:04:54 +0100

    feat(fishandchips): improve readability by removing end of lines requirements

--- solutions/fishandchips.go
+++ solutions/fishandchips.go
@@ -2,17 +2,17 @@ package solutions
 
 func FishAndChips(n int) string {
 	if n < 0 {
-		return "error: number is negative\n"
+		return "error: number is negative"
 	}
 
 	if (n%2 == 0) && (n%3 == 0) {
-		return "fish and chips\n"
+		return "fish and chips"
 	}
 	if n%2 == 0 {
-		return "fish\n"
+		return "fish"
 	}
 	if n%3 == 0 {
-		return "chips\n"
+		return "chips"
 	}
-	return "error: non divisible\n"
+	return "error: non divisible"
 }
