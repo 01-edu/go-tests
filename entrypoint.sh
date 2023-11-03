4595e8ac8555c0bc83da2bb630bdd92f20e4face
lee <leedasilva988@gmail.com>
2023-11-03 12:01:00 +0000

    fix(entrypoint): init module with main instead of piscine

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ cp -r student piscine-go
 cd piscine-go
 
 if test "$CODE_EDITOR_MODE"; then
-	go mod init piscine 2>/dev/null
+	go mod init main 2>/dev/null
 	GOSUMDB=off go get github.com/01-edu/z01@v0.1.0 2>/dev/null
 fi
 
