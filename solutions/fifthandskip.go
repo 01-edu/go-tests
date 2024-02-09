2def13ed2481f7b9bb048771e3a74b4199797dfe
miguel <miguel-silva98@hotmail.com>
2024-02-09 17:38:23 +0000

    fix(fifthandskip) fix the solution

--- solutions/fifthandskip.go
+++ solutions/fifthandskip.go
@@ -12,15 +12,15 @@ func FifthAndSkip(str string) string {
 	s := strings.ReplaceAll(str, " ", "")
 	var _str strings.Builder
 	j := 0
-	for i := 0; i < len(s); i++ {
+	for _, char := range s {
 		if j == 5 {
 			_str.WriteRune(rune(' '))
 			j = 0
 		} else {
-			_str.WriteRune(rune(s[i]))
+			_str.WriteRune(rune(char))
 			j++
 		}
 	}
-	_str.WriteRune(rune('\n'))
-	return (_str.String())
+	_str.WriteRune('\n')
+	return _str.String()
 }
