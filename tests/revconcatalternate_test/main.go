f5e979202e48cd9f70634fc01cd466a6b086c646
nprimo <primo.niccolo@gmail.com>
2024-03-11 14:40:41 +0000

    refactor(revconcatalternate): change test comparison
    - Compare output of submitted function with expected result instead of
      reference function's output.

--- tests/revconcatalternate_test/main.go
+++ tests/revconcatalternate_test/main.go
@@ -1,24 +1,62 @@
 package main
 
 import (
+	"log"
+	"reflect"
 	student "student"
-
-	"github.com/01-edu/go-tests/lib/challenge"
-	"github.com/01-edu/go-tests/solutions"
 )
 
+var testCases = []struct {
+	args [][]int
+	want []int
+}{
+	{
+		[][]int{{1, 2, 3}, {4, 5, 6}},
+		[]int{3, 6, 2, 5, 1, 4},
+	},
+	{
+		[][]int{{4, 5, 6, 7, 8, 9}, {1, 2, 3}},
+		[]int{9, 8, 7, 6, 3, 5, 2, 4, 1},
+	},
+	{
+		[][]int{{1, 2, 3}, {4, 5, 6, 7, 8, 9}},
+		[]int{9, 8, 7, 3, 6, 2, 5, 1, 4},
+	},
+	{
+		[][]int{{1, 2, 3}, {4, 5}},
+		[]int{3, 2, 5, 1, 4},
+	},
+	{
+		[][]int{{}, {4, 5, 6}},
+		[]int{6, 5, 4},
+	},
+	{
+		[][]int{{1, 2, 3}, {}},
+		[]int{3, 2, 1},
+	},
+	{
+		[][]int{{}, {}},
+
+		[]int{},
+	},
+	{
+		[][]int{{1, 2, 4}, {10, 20, 30, 40, 50}},
+
+		[]int{50, 40, 4, 30, 2, 20, 1, 10},
+	},
+}
+
 func main() {
-	args := [][][]int{
-		{{1, 2, 3}, {4, 5, 6}},
-		{{4, 5, 6, 7, 8, 9}, {1, 2, 3}},
-		{{1, 2, 3}, {4, 5, 6, 7, 8, 9}},
-		{{1, 2, 3}, {4, 5}},
-		{{}, {4, 5, 6}},
-		{{1, 2, 3}, {}},
-		{{}, {}},
-		{{1, 2, 4}, {10, 20, 30, 40, 50}},
-	}
-	for _, arg := range args {
-		challenge.Function("RevConcatAlternate", student.RevConcatAlternate, solutions.RevConcatAlternate, arg[0], arg[1])
+	for _, tc := range testCases {
+		got := student.RevConcatAlternate(tc.args[0], tc.args[1])
+		if !reflect.DeepEqual(got, tc.want) {
+			log.Fatalf("%s(%#v, %#v) == %#v instead of %#v\n",
+				"RevConcatAlternate",
+				tc.args[0],
+				tc.args[1],
+				got,
+				tc.want,
+			)
+		}
 	}
 }
