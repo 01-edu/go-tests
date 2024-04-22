e326ae989c2b69a0b6860e0b3164e7dbcd190799
miguel <miguel-silva98@hotmail.com>
2024-04-22 18:49:13 +0100

    feat(canjump):solution and test

---
+++ solutions/canjump.go
@@ -0,0 +1,26 @@
+package solutions
+
+func CanJump(steps ...int) bool {
+	if len(steps) == 0 {
+		return false
+	}
+
+	maxReach := 0
+	for i := 0; i < len(steps); i++ {
+		if i > maxReach {
+			return false
+		}
+		maxReach = max(maxReach, i+steps[i])
+		if maxReach == len(steps)-1 || len(steps) == 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ tests/canjump_test/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"reflect"
+	student "student"
+)
+
+func main() {
+	tests := []struct {
+		args []int
+		want bool
+	}{
+		{args: []int{2, 3, 1, 1, 4}, want: true},
+		{args: []int{1, 1, 1, 1, 0}, want: true},
+		{args: []int{5, 4, 3, 2, 1, 0}, want: true},
+		{args: []int{0}, want: true},
+		{args: []int{5}, want: true},
+		{args: []int{}, want: false},
+		{args: []int{1, 2, 3, 0, 2}, want: false},
+		{args: []int{3, 2, 1, 0, 4}, want: false},
+		{args: []int{0, 0, 0, 0, 0}, want: false},
+		{args: []int{1, 2, 3}, want: false},
+		{args: []int{1, 2, 3, 0, 1}, want: false},
+		{args: []int{1, 0, 0, 0, 0}, want: false},
+		{args: []int{1, 0, 1, 0, 1}, want: false},
+		{args: []int{10, 20, 30, 40, 0}, want: false},
+	}
+
+	for _, tc := range tests {
+		got := student.CanJump(tc.args...)
+		if !reflect.DeepEqual(got, tc.want) {
+			log.Fatalf("%s(%#v) == %#v instead of %#v\n",
+				"CanJump",
+				tc.args,
+				got,
+				tc.want,
+			)
+		}
+	}
+}
